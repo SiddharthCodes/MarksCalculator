f56862b5ac38462d0757b16b8dc1b1e702e3bb68
Astronautyc <155202212+Astronautyc@users.noreply.github.com>
2024-11-21 04:12:31 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Button1 = New Button()
+        Label6 = New Label()
+        Label7 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(63, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(46, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Physics"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(63, 66)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(61, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Chemistry"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(63, 103)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(47, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Biology"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(63, 133)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(76, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Mathematics"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(63, 168)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(66, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "Computers"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(192, 27)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(81, 23)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(192, 63)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(81, 23)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(192, 95)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(81, 23)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(192, 133)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(81, 23)
+        TextBox4.TabIndex = 8
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(192, 162)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(81, 23)
+        TextBox5.TabIndex = 9
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(192, 230)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(120, 26)
+        Button1.TabIndex = 10
+        Button1.Text = "Calculate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(63, 280)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(66, 15)
+        Label6.TabIndex = 11
+        Label6.Text = "Percentage"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(63, 329)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(38, 15)
+        Label7.TabIndex = 12
+        Label7.Text = "Grade"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Button1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Declare variables and input values
+        Dim marks(4) As Double
+        marks(0) = Val(TextBox1.Text) ' Physics
+        marks(1) = Val(TextBox2.Text) ' Chemistry
+        marks(2) = Val(TextBox3.Text) ' Biology
+        marks(3) = Val(TextBox4.Text) ' Mathematics
+        marks(4) = Val(TextBox5.Text) ' Computer
+
+        ' Calculate total and percentage
+        Dim total As Double = marks.Sum()
+        Dim percentage As Double = (total / 500) * 100
+
+        ' Display percentage
+        Label6.Text = "Percentage: " & percentage.ToString("F2") & "%"
+
+        ' Determine grade
+        Dim grade As String = If(percentage >= 90, "A", If(percentage >= 80, "B", If(percentage >= 70, "C", If(percentage >= 60, "D", If(percentage >= 40, "E", "F")))))
+        Label7.Text = "Grade: " & grade
+    End Sub
+End Class
